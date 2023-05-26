94bbfe29787cccf608863d07cb9fcf02ac7c7d28
escowin <edwin@escowinart.com>
2023-05-26 13:28:56 -0500

    tested out useState in workout form

--- client/src/components/WorkoutForm.js
+++ client/src/components/WorkoutForm.js
@@ -1,22 +1,35 @@
+import React, { useState } from "react";
+
 import { useMutation } from "@apollo/client";
 import { ADD_WORKOUT } from "../utils/mutations";
 
 function WorkoutForm() {
-  // useMutation
+  const [notes, setNotes] = useState("");
+  const [characterCount, setCharacterCount] = useState(0);
+
+  // addWorkout runs the mutation
   const [addWorkout, { error }] = useMutation(ADD_WORKOUT);
+
+  // update state based on form input changes
+  const handleChange = (event) => {
+    if (event.target.value.length <= 80) {
+      setNotes(event.target.value);
+      setCharacterCount(event.target.value.length);
+    }
+  };
 
   const handleFormSubmit = async (e) => {
     e.preventDefault();
 
     try {
-      // adds workout to database
-      // await addWorkout({
-      //   variables: { activity, meters, adjustedMeters, date, notes },
-      // });
+      // adds workout through the function
+      await addWorkout({
+        variables: { notes },
+      });
 
-      // clears form values
-      // setText("");
-      // setCharacterCount(0);
+      //   // clears form values
+      //   setText("");
+      //   setCharacterCount(0);
     } catch (err) {
       console.error(err);
     }
@@ -25,14 +38,27 @@ function WorkoutForm() {
   return (
     <form onSubmit={handleFormSubmit}>
       <h2>Add workout</h2>
+
       <label htmlFor="activity">activity</label>
       <input type="text" id="activity"></input>
+
       <label htmlFor="date">date</label>
       <input type="date" id="date"></input>
+
       <label htmlFor="meters">meters</label>
       <input type="number" id="meters"></input>
-      <label htmlFor="notes">notes</label>
-      <input type="text" id="notes"></input>
+
+      <label htmlFor="notes">
+        notes
+        <span
+          className={`${characterCount === 80 || error ? "text-error" : ""}`}
+        >
+          {characterCount}/80
+          {error && <span>error</span>}
+        </span>
+      </label>
+      <input type="text" id="notes" value={notes} onChange={handleChange}></input>
+
       <button type="submit">add</button>
     </form>
   );
