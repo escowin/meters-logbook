0b8ded49a00118fde9c044b1f608904c90897e83
escowin <edwin@escowinart.com>
2023-12-11 10:56:14 -0600

    applied mvp styles to workout page component

--- client/src/pages/Workout.js
+++ client/src/pages/Workout.js
@@ -2,18 +2,18 @@ import { useEffect } from "react";
 import { useParams } from "react-router-dom";
 import { useQuery } from "@apollo/client";
 import { QUERY_WORKOUT } from "../utils/queries";
-import Auth from "../utils/auth";
-import "../assets/styles/workout.css"
+import "../assets/styles/workout.css";
 
 function Workout({ setMain }) {
-  const loggedIn = Auth.loggedIn();
   const { id: workoutId } = useParams();
   const { loading, data } = useQuery(QUERY_WORKOUT, {
     variables: { id: workoutId },
   });
   const workout = data?.workout || {};
 
-  useEffect(() => setMain("workout"), [setMain])
+  useEffect(() => setMain("workout"), [setMain]);
+
+  // to-do: implement edit & delete options
 
   if (loading) {
     return <section>Loading...</section>;
@@ -23,11 +23,6 @@ function Workout({ setMain }) {
     <>
       <section id="workout">
         <h2>{workout.activity} </h2>
-        {loggedIn ? (
-          <>
-            <p>User</p> <p>{workout.username}</p>
-          </>
-        ) : null}
         <p>Date</p> <p>{workout.date}</p>
         <p>Meters</p> <p>{workout.meters}m</p>
         <p>Adjusted</p>
@@ -36,10 +31,12 @@ function Workout({ setMain }) {
         ) : (
           <p>-</p>
         )}
-      </section>
-      <section id="note-section">
-        <h2>Notes</h2>
+        <p>Notes</p>
         <p>{workout.notes}</p>
+      </section>
+      <section id="options">
+        <button onClick={() => console.log("edit")}>edit</button>
+        <button onClick={() => console.log("delete")}>delete</button>
       </section>
     </>
   );
